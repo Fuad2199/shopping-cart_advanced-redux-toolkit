08e70f18cba15c06af54f4064bc71a95fecb86fb
Fuad2199 <fuad.beybutov099@gmail.com>
2025-02-28 18:47:00 +0400

    update redux

---
+++ front-end/src/App/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import productsReducer, { productsFetch } from '../slices/productSlice'
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+})
+
+store.dispatch(productsFetch)
